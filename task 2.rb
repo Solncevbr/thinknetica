00b3b6cbbfc535cc66d9fa31ddce16bd030fa3c8
Solncevbr <48673798+Solncevbr@users.noreply.github.com>
2019-03-21 13:40:26 +0200

    Update task 2.rb

--- task 2.rb
+++ task 2.rb
@@ -1,10 +1,6 @@
-def triangle_area(area, height)
-  0.5 * area * height
-end
-
 puts "Введите основание треугольника: "
 area = gets.to_f
 puts "Введите высоту треугольника: "
 height = gets.to_f
-
-puts "Площадь треугольника #{triangle_area(area, height)}"
+triangle_area = (area * height)/2
+puts "Площадь треугольника #{triangle_area}"
